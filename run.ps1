a0cbc1a6ee23ad1648ac447478f9b80f53366479
Yurii <juretz32@yandex.ru>
2020-04-05 00:21:08 +0300

    Specify HEAP setting

--- run.ps1
+++ run.ps1
@@ -8,6 +8,11 @@ param(
     $ConfigFile
 )
 
+
+${env:HEAP} = '-Xms64m -Xmx64m -XX:MaxMetaspaceSize=256m'
+
 $ConfigFile = Get-Item $ConfigFile
+
 $report_dir = $ConfigFile.Directory
+
 jmeter -n -t $TestPlan -p $ConfigFile -e -l (Join-Path $report_dir 'jmeter.log') -o (Join-Path $report_dir 'report')
